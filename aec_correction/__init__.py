18ad88e386ea5862667f86253f1c686a99bfeb59
WJ714 <66468131+WJ714@users.noreply.github.com>
2024-01-08 21:52:58 +0100

    Add files via upload

---
+++ aec_correction/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'aec_correction'
+__version__ = '1.0.0'
+__author__ = 'Weijie Zhang'
+__author_email__ = 'wzhang@bgc-jena.mpg.de'
+__license__ = 'GNU GPLv3'
+__copyright__ = 'Copyright 2024 by Weijie Zhang, MPI-BGC-GDM'
